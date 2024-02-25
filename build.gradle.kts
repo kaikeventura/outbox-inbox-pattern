d0aff1aedc81523fdc4f46251b9e8cd53fc18fc6
kaikeventura <kaike.gnr@hotmail.com>
2024-02-25 11:44:17 -0300

    Incluso config do mysql e kafka

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,8 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.kafka:spring-kafka")
 	runtimeOnly("com.mysql:mysql-connector-j")
--- src/main/kotlin/com/kaikeventura/outboxinboxpattern/OutboxInboxPatternApplication.kt
+++ src/main/kotlin/com/kaikeventura/outboxinboxpattern/OutboxInboxPatternApplication.kt
@@ -2,8 +2,10 @@ package com.kaikeventura.outboxinboxpattern
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@EnableJpaRepositories
 class OutboxInboxPatternApplication
 
 fun main(args: Array<String>) {
